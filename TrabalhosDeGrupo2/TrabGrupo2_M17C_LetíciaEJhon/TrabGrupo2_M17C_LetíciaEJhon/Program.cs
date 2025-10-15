a23c9593062682c7c7b0ab51af8e89937538d6ce
Letícia Rodrigues <12.leticia.rodrigues@gmail.com>
2025-10-15 16:09:24 +0100

    1.ºCommit

---
+++ TrabalhosDeGrupo2/TrabGrupo2_M17C_LetíciaEJhon/TrabGrupo2_M17C_LetíciaEJhon/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace TrabGrupoM17C_LetíciaEJhon
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] vetor = new int[10];
+            Console.WriteLine("Indice {1}. ", vetor);
+            int maiornumero = 0;
+            Console.WriteLine("Maximo{1}. ", maiornumero);
+            for (int i = 0; i < vetor.Length; i++)
+            {
+                Console.WriteLine("introduza o " + (i + 1) + "º numero");
+                vetor[i] = int.Parse(Console.ReadLine());
+            }
+
+
+
+            for (int i = 1; i < vetor.Length; i++)
+            {
+                if (vetor[i] > vetor[maiornumero])
+                {
+                    maiornumero = i;
+                }
+            }
+            Console.WriteLine("O maior numero é " + vetor[maiornumero] + " e esta na posiçao " + maiornumero + 1 + "º de 1-10");
+        }
+    }
+}
